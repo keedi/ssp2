33fb11d63be54891d1454aefc98c122c8c3108d5
Keedi Kim <keedi.k@gmail.com>
2017-03-06 19:35:36 +0900

    Modularize the code

--- 32-rhum.pl
+++ 32-rhum.pl
@@ -4,185 +4,105 @@ use utf8;
 use strict;
 use warnings;
 
-use Path::Tiny;
+use SSP2::Iter;
+use SSP2::Util;
 
-my $ndays = 365;
-my $ncols = 751;
-my $nrows = 601;
-
-my $output = "32-rhum.dat";
-
-#my $data_dir = "/mnt/500g/SHARE/dr.park/data/East_Asia/grid1kmData_ascii";
 my $data_dir = "W:/grid1kmData_ascii";
-
-my $term = "daily";
-my $var  = "rhum";
-my $year = 2006;
-
-# ex:
-#     linux:    /mnt/500g/SHARE/dr.park/data/East_Asia/grid1kmData_ascii/daily/tavg/2006/
-#     windows:                                      W:/grid1kmData_ascii/daily/rhum/2006/ssp2_rhum_daily_2006-001_1kmgrid.txt
-my $file_fmt = sprintf(
-    "%s/%s/%s/%d/ssp2_%s_%s_%d-%%03d_1kmgrid.txt",
-    $data_dir,
-    $term,
-    $var,
-    $year,
-    $var,
-    $term,
-    $year,
-);
+my $term     = "daily";
+my $var      = "rhum";
+my $year     = 2006;
+my $ndays    = 365;
+my $ncols    = 751;
+my $nrows    = 601;
+my $output   = "32-rhum.dat";
 
 my @files;
-for ( my $i = 1; $i <= $ndays; ++$i ) {
-    my $file = sprintf( $file_fmt, $i );
-    push @files, $file;
+{
+    my $file_fmt = sprintf(
+        "%s/%s/%s/%d/ssp2_%s_%s_%d-%%03d_1kmgrid.txt",
+        $data_dir,
+        $term,
+        $var,
+        $year,
+        $var,
+        $term,
+        $year,
+    );
+
+    for ( my $i = 1; $i <= $ndays; ++$i ) {
+        my $file = sprintf( $file_fmt, $i );
+        push @files, $file;
+    }
 }
 
-my %params = (
-    header   => [],
-    result   => $result,
-    ndays    => $ndays,
-    nrows    => $nrows,
-    ncols    => $ncols,
-    files    => \@files,
-    cb_init  => sub {
-        my ( $params ) = @_;
+my $si = SSP2::Iter->new(
+    ndays   => $ndays,
+    ncols   => $ncols,
+    nrows   => $ndays,
+    files   => \@files,
+    result  => [],
+    cb_init => sub {
+        my $self = shift;
 
-        for ( my $i = 0; $i < $params->{nrows}; ++$i ) {
-            $params->{result}[$i] = [];
-            for ( my $j = 0; $j < $params->{ncols}; ++$j ) {
-                $params->{result}[$i][$j] = {
+        for ( my $i = 0; $i < $self->nrows; ++$i ) {
+            $self->result->[$i] = [];
+            for ( my $j = 0; $j < $self->ncols; ++$j ) {
+                $self->result->[$i][$j] = {
                     val => 0,
                     cnt => 0,
                 };
             }
         }
     },
-    cb       => sub {
-        my ( $params, $rows, $cols, $item ) = @_;
+    cb => sub {
+        my ( $self, $rows, $cols, $item ) = @_;
 
-        if ( $item != -9999 ) {
-            $params->{result}[$rows][$cols]{val} += $item;
-            ++$params->{result}[$rows][$cols]{cnt};
-        }
+        return if $item == $self->ndv;
+
+        #
+        # sum and count
+        #
+        $self->result->[$rows][$cols]{val} += $item;
+        ++$self->result->[$rows][$cols]{cnt};
     },
     cb_final => sub {
-        my ( $params ) = @_;
+        my $self = shift;
 
-        for ( my $i = 0; $i < $params->{nrows}; ++$i ) {
-            for ( my $j = 0; $j < $params->{ncols}; ++$j ) {
-                my $cnt = $params->{result}[$i][$j]{cnt};
-                my $val = $params->{result}[$i][$j]{val};
+        #
+        # avg
+        #
+        for ( my $i = 0; $i < $self->nrows; ++$i ) {
+            for ( my $j = 0; $j < $self->ncols; ++$j ) {
+                my $cnt = $self->result->[$i][$j]{cnt};
+                my $val = $self->result->[$i][$j]{val};
                 if ( $cnt > 0 ) {
-                    $params->{result}[$i][$j]{avg} = $val / $cnt;
+                    $self->result->[$i][$j]{avg} = $val / $cnt;
                 }
                 else {
-                    $params->{result}[$i][$j]{avg} = undef;
+                    $self->result->[$i][$j]{avg} = undef;
                 }
             }
+        }
+
+        #
+        # write
+        #
+        my $fh = path($output)->filehandle( ">", ":raw:encoding(UTF-8)" );
+        print $fh "$_\n" for @{ $self->headers };
+        for ( my $i = 0; $i < $self->nrows; ++$i ) {
+            for ( my $j = 0; $j < $self->ncols; ++$j ) {
+                my $avg = $self->result->[$i][$j]{avg};
+                if ( defined $avg ) {
+                    printf $fh "%.2f", $avg;
+                }
+                else {
+                    print $fh $self->ndv;
+                }
+                print $fh q{ };
+            }
+            print $fh "\n";
         }
     },
 );
 
-iter( \%params );
-
-my $ndv = no_data_value( $params{header} );
-warn "no data value: $ndv\n";
-
-{
-    my $fh = path($output)->filehandle( ">", ":raw:encoding(UTF-8)" );
-
-    print $fh "$_\n" for @{ $params{header} };
-    for ( my $i = 0; $i < $nrows; ++$i ) {
-        for ( my $j = 0; $j < $ncols; ++$j ) {
-            my $avg = $result->[$i][$j]{avg};
-            if ( defined $avg ) {
-                printf $fh "%.2f", $avg;
-            }
-            else {
-                print $fh $ndv;
-            }
-            print $fh q{ };
-        }
-        print $fh "\n";
-    }
-}
-
-sub no_data_value {
-    my $headers = shift;
-
-    my $ndv;
-    for my $header ( @$headers ) {
-        next unless $header =~ m/^NODATA_value\s(\S+)/;
-        $ndv = $1;
-    }
-
-    return $ndv;
-}
-
-sub iter {
-    my $params = shift;
-
-    $params->{cb_init}->($params);
-
-    my @header_strings;
-    for my $file ( @{ $params->{files} } ) {
-        warn "processing $file\n";;
-
-        my $path = path($file);
-        warn "file does not exists: $file\n" unless $path->exists;
-        my $fh = $path->filehandle( "<", ":raw:encoding(UTF-8)" );
-
-        my %header = (
-            ncols        => 0,
-            nrows        => 0,
-            xllcorner    => undef,
-            yllcorner    => undef,
-            cellsize     => undef,
-            NODATA_value => undef,
-        );
-        my $header_keys = join "|", keys %header;
-        my $regex_kv = qr/^($header_keys)\s+(\S+)$/;
-        my $rows = 0;
-        @header_strings = ();
-        while (<$fh>) {
-            portable_chomp();
-
-            if ( m/$regex_kv/ ) {
-                my $k = $1;
-                my $v = $2;
-                $header{$k} = $v;
-                push @header_strings, $_;
-                next;
-            }
-
-            my @items = split;
-            my $cols = @items;
-            die "invalid cols row($rows): $cols == $params->{ncols}" unless $cols == $params->{ncols};
-
-            $cols = 0;
-            for my $item (@items) {
-                $params->{cb}->( $params, $rows, $cols, $item );
-                ++$cols;
-            }
-
-            ++$rows;
-        }
-        die "invalid ncols: $params->{ncols} == $header{ncols}\n" unless $params->{ncols} == $header{ncols};
-        die "invalid nrows: $params->{nrows} == $header{nrows}\n" unless $params->{nrows} == $header{nrows};
-
-        close $fh;
-    }
-
-    $params->{header} = \@header_strings;
-
-    $params->{cb_final}->($params);
-}
-
-sub portable_chomp {
-    my $line = shift;
-
-    $_ //= $line;
-    s/(?:\x{0d}?\x{0a}|\x{0d})$//; # chomp
-}
+$si->iter;
---
+++ lib/SSP2/Iter.pm
@@ -0,0 +1,121 @@
+package SSP2::Iter;
+# ABSTRACT: SSP2 iter
+
+use utf8;
+
+use Moo;
+use Types::Standard qw( ArrayRef CodeRef Int );
+use Types::Path::Tiny qw( File );
+
+use SSP2::Util;
+
+our $VERSION = '0.001';
+
+has ndays    => ( is => "ro",  isa => Int, required => 1 );
+has nrows    => ( is => "ro",  isa => Int, required => 1 );
+has ncols    => ( is => "ro",  isa => Int, required => 1 );
+has files    => ( is => "ro",  isa => ArrayRef[File], required => 1 );
+has headers  => ( is => "rwp", isa => ArrayRef );
+has cb_init  => ( is => "rw",  isa => CodeRef );
+has cb_final => ( is => "rw",  isa => CodeRef );
+has cb       => ( is => "rw",  isa => CodeRef );
+has ndv      => ( is => "rwp", isa => Int );
+has result   => ( is => "rw" );
+
+sub iter {
+    my $self = shift;
+
+    $self->cb_init->($self) if $self->cb_init;
+
+    for my $file ( @{ $self->files } ) {
+        warn "processing $file\n";;
+
+        die "file does not exists: $file\n" unless $file->exists;
+        my $fh = $file->filehandle( "<", ":raw:encoding(UTF-8)" );
+
+        my %header = (
+            ncols        => 0,
+            nrows        => 0,
+            xllcorner    => undef,
+            yllcorner    => undef,
+            cellsize     => undef,
+            NODATA_value => undef,
+        );
+        my $header_keys = join "|", keys %header;
+        my $regex_kv = qr/^($header_keys)\s+(\S+)$/;
+        my $rows = 0;
+		$self->_set_headers( [] );
+        while (<$fh>) {
+            SSP2::Util::portable_chomp();
+
+            if ( m/$regex_kv/ ) {
+                my $k = $1;
+                my $v = $2;
+                $header{$k} = $v;
+                push @{ $self->headers }, $_;
+                $self->_set_ndv($v) if $k eq "NODATA_value";
+                next;
+            }
+
+            my @items = split;
+            my $cols = @items;
+            die( sprintf "invalid cols row(%d): %d == %d", $rows, $cols, $self->ncols )
+                unless $cols == $self->ncols;
+
+            $cols = 0;
+            for my $item (@items) {
+                $self->cb->( $self, $rows, $cols, $item ) if $self->cb;
+                ++$cols;
+            }
+
+            ++$rows;
+        }
+
+        die( sprintf "invalid ncols: %d == %d\n", $self->ncols, $header{ncols} )
+            unless $self->ncols == $header{ncols};
+        die( sprintf "invalid nrows: %d == %d\n", $self->nrows, $header{nrows} )
+            unless $self->nrows == $header{nrows};
+
+        close $fh;
+    }
+
+    $self->cb_final->($self) if $self->cb_final;
+}
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=for Pod::Coverage BUILDARGS
+
+=head1 SYNOPSIS
+
+    ...
+
+
+=head1 DESCRIPTION
+
+...
+
+
+=attr ndays
+
+=attr nrows
+
+=attr ncols
+
+=attr files
+
+=attr headers
+
+=attr cb_init
+
+=attr cb_final
+
+=attr cb
+
+=attr result
+
+=method iter
