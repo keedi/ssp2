702bbbee7969c33468bdfcba51d907d8549802cc
Keedi Kim <keedi.k@gmail.com>
2017-03-08 03:07:25 +0900

    Add utility to convert utf-8 file to cp949

---
+++ utf8-to-cp949.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+use utf8;
+use strict;
+use warnings;
+use feature qw( say );
+
+use Path::Tiny;
+
+my ( $arg_src, $arg_dest ) = @ARGV;
+die "Usage: $0 <src_dir> <dest_dir>\n" unless $arg_src && -d $arg_src && $arg_dest;
+
+my $src  = path($arg_src);
+my $dest = path($arg_dest);
+
+$dest->mkpath;
+for my $s ( $src->children ) {
+    next unless $s->is_file;
+
+    my $d = $dest->child( $s->basename );
+
+    say "$s -> $d";
+
+    my $sfh = $s->filehandle( "<", ":raw:encoding(UTF-8)" );
+    my $dfh = $d->filehandle( ">", ":raw:encoding(cp949)" );
+    while (<$sfh>) {
+        print {$dfh} $_;
+    }
+    close $dfh;
+    close $sfh;
+}
