c8086422edb8a26df4a958e2c9a5956077a52718
Keedi Kim <keedi.k@gmail.com>
2017-03-08 18:25:41 +0900

    Add 19-tavg, 20-tmax, 21-tmin script

---
+++ 20-tmax.pl
@@ -0,0 +1,182 @@
+#!/usr/bin/env perl
+
+use utf8;
+use strict;
+use warnings;
+use feature qw( state );
+
+use Benchmark;
+use Path::Tiny;
+use Try::Tiny;
+
+use SSP2::Iter;
+use SSP2::Log;
+use SSP2::Sigungu;
+
+local $| = 1;
+
+my $log_file = shift;
+die "Usage: $0 <log_file>\n" unless $log_file;
+
+my $LOG = SSP2::Log->new( file => $log_file );
+
+my @years = (
+    2006 .. 2025,
+    2046 .. 2065,
+    2080 .. 2099,
+);
+
+for my $year (@years) {
+    $LOG->info("processing $year");
+    my $t0 = Benchmark->new;
+    try {
+        doit($year);
+    }
+    catch {
+        $LOG->warn("caught error: $_");
+    };
+    my $t1 = Benchmark->new;
+    my $td = timediff( $t1, $t0 );
+    $LOG->info( "elapsed time: %s", timestr($td) );
+}
+
+sub doit {
+    my $year = shift;
+
+    return unless $year;
+
+    my $data_dir = "W:/grid1kmData_ascii";
+    my $term     = "daily";
+    my $var      = "tmax";
+    my $ndays    = 365;
+    my $ncols    = 751;
+    my $nrows    = 601;
+    my $output   = "W:/ssp2/result/20-${var}/SSP2_${var}-1d-avg_${term}_CityLevel_${year}_sub.csv";
+    my $encoding = "cp949";
+
+    if ( path($output)->is_file ) {
+        $LOG->info("skip: $output file is already exists");
+        return;
+    }
+
+    state $ss = SSP2::Sigungu->new(
+        ncols         => $ncols,
+        nrows         => $nrows,
+        info_file     => "W:/define/sigungu230_info.txt",
+        boundary_file => "W:/define/sigungu230_boundary.txt",
+    );
+
+    my @files;
+    {
+        my $file_fmt = sprintf(
+            "%s/%s/%s/%d/ssp2_%s_%s_%d-%%03d_1kmgrid.txt",
+            $data_dir,
+            $term,
+            $var,
+            $year,
+            $var,
+            $term,
+            $year,
+        );
+
+        for ( my $i = 1; $i <= $ndays; ++$i ) {
+            my $file = sprintf( $file_fmt, $i );
+            push @files, $file;
+        }
+    }
+
+    my $si = SSP2::Iter->new(
+        ncols   => $ncols,
+        nrows   => $nrows,
+        files   => \@files,
+        result  => [],
+        cb_init => sub {
+            my $self = shift;
+
+            for ( my $i = 0; $i < @{ $self->files }; ++$i ) {
+                $self->result->[$i] = {};
+                for my $code ( $ss->codes ) {
+                    $self->result->[$i]{$code} = {
+                        val => 0,
+                        cnt => 0,
+                    };
+                }
+            }
+        },
+        cb_file_init => sub {
+            my ( $self, $file_idx, $file ) = @_;
+
+            #
+            # debug log
+            #
+            $LOG->debug("processing $file");
+        },
+        cb_file_retry => sub {
+            my ( $self, $file_idx, $file, $retry, $msg ) = @_;
+
+            $LOG->warn($msg);
+            $LOG->debug( "retry(%d/%d): $file", $retry, $self->retry );
+
+            for my $code ( $ss->codes ) {
+                $self->result->[$file_idx]{$code} = {
+                    val => 0,
+                    cnt => 0,
+                };
+            }
+        },
+        cb => sub {
+            my ( $self, $file_idx, $row, $col, $item ) = @_;
+
+            return if $item == $self->ndv;
+
+            my ( $code, $nm1, $nm2 ) = $ss->rowcol2info( $row, $col );
+            return unless $code;
+
+            $self->result->[$file_idx]{$code}{val} += $item;
+            ++$self->result->[$file_idx]{$code}{cnt};
+        },
+        cb_final => sub {
+            my $self = shift;
+
+            #
+            # daily sigungu avg
+            #
+            for ( my $i = 0; $i < @{ $self->files }; ++$i ) {
+                for my $code ( $ss->codes ) {
+                    my $val = $self->result->[$i]{$code}{val};
+                    my $cnt = $self->result->[$i]{$code}{cnt};
+                    if ( $cnt > 0 ) {
+                        $self->result->[$i]{$code}{avg} = $val / $cnt;
+                    }
+                    else {
+                        $self->result->[$i]{$code}{avg} = undef;
+                    }
+                }
+            }
+
+            #
+            # write
+            #
+            my $output_path = path($output);
+            $output_path->parent->mkpath;
+            my $fh = $output_path->filehandle( ">", ":raw:encoding($encoding)" );
+            print $fh join( "\t", $ss->daily_headers($year) ) . "\n";
+            for my $code ( $ss->codes ) {
+                my @items = ( $code, $ss->nm2($code), $ss->nm1($code) );
+                for ( my $i = 0; $i < @{ $self->files }; ++$i ) {
+                    my $avg = $self->result->[$i]{$code}{avg};
+                    if ( defined $avg ) {
+                        push @items, sprintf("%f", $avg);
+                    }
+                    else {
+                        push @items, $self->ndv;
+                    }
+                }
+                print $fh join( "\t", @items ) . "\n";
+            }
+            close $fh;
+        },
+    );
+
+    $si->iter;
+}
